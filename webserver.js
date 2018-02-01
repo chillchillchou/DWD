e0c3d72ead56a3c5dccfd8656ed379da52cf1964
chillchillchou <huiyiche@usc.edu>
2018-02-01 14:18:22 -0500

    added something else

--- webserver.js
+++ webserver.js
@@ -5,6 +5,11 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 })
 
+app.get('/somethingelse', function (req, res) {
+  res.send('somethingelse!');
+})
+
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 })
