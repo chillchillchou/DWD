db9521795dde60b84fb0a214bf5a86ab60e891df
chillchillchou <huiyiche@usc.edu>
2018-02-03 00:41:11 -0500

    add comment and log to webserver

--- week2/webserver.js
+++ week2/webserver.js
@@ -1,13 +1,23 @@
 var express = require('express');
 var app = express();
 
+//Every time the app receives a request, it prints the message “LOGGED” to the terminal.
+var myLogger = function(req,res,next){
+  console.log("LOGGED");
+  next();
+}
+
+//load the middleware function
+app.use(myLogger);
+//middleware functions that are loaded first are also executed first.
 app.get('/', function (req, res) {
   res.send('Hello World!');
 })
 
 app.get('/somethingelse', function (req, res) {
+//this is middleware
   res.send('somethingelse!');
-})
+});
 
 
 app.listen(3000, function () {
