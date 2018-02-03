4aa4bcc9df896c4a9955a4284490cc7913770c26
chillchillchou <huiyiche@usc.edu>
2018-02-02 20:31:12 -0500

    add week1 and week2

---
+++ week1/server.js
@@ -0,0 +1,31 @@
+// HTTP Portion
+var http = require('http');
+var fs = require('fs'); // Using the filesystem module
+var httpServer = http.createServer(requestHandler);
+var url = require('url');
+httpServer.listen(8080);
+
+function requestHandler(req, res) {
+
+	var parsedUrl = url.parse(req.url);
+	console.log("The Request is: " + parsedUrl.pathname);
+
+	fs.readFile(__dirname + parsedUrl.pathname,
+		// Callback function for reading
+		function (err, data) {
+			// if there is an error
+			if (err) {
+				res.writeHead(500);
+				return res.end('Error loading ' + parsedUrl.pathname);
+			}
+			// Otherwise, send the data, the contents of the file
+			res.writeHead(200);
+			res.end(data);
+  		}
+  	);
+
+  	/*
+  	res.writeHead(200);
+  	res.end("Life is wonderful");
+  	*/
+}
