108227e67e9bdf1c02b41e73fa89c5ca94f54c41
chillchillchou <huiyiche@usc.edu>
2018-02-13 02:31:38 -0500

    comment out configure

--- week2/webserver.js
+++ week2/webserver.js
@@ -1,4 +1,4 @@
-var config = require("./config.js")
+//var config = require("./config.js")
 // var mongojs=require('mongojs');
 // var db=mongojs("subuser:cho@s043350.mlab.com:43350/testdatabase")
 var express = require('express');
