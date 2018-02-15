5a2eb26ed6ea61e6e3e7563600429ea8a52e8026
chillchillchou <huiyiche@usc.edu>
2018-02-14 20:11:27 -0500

    change require config

--- week3/webserver.js
+++ week3/webserver.js
@@ -1,4 +1,4 @@
-var config = require('config.js');
+var config = require('./config.js');
 var mongojs = require('mongojs');
 var db = mongojs("config.username:config.password@ds043350.mlab.com:43350/testdatabase", ['truth']);
 var express = require('express');
