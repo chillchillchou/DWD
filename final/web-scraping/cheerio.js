1ffeabfbb30b051d30d5608bf6a5fe624c53a0d8
chenhuiyiorange <huiyichen1993@gmail.com>
2018-03-05 13:53:17 -0500

    add final folder

---
+++ final/web-scraping/cheerio.js
@@ -0,0 +1,80 @@
+var cheerio = require('cheerio');
+var fs = require('fs');
+var mongojs = require('mongojs');
+var db = mongojs("cho:woshinaochou4@ds235768.mlab.com:35768/truthbook", ['yahoo']);
+var express = require('express');
+//
+// var htmlString = fs.readFileSync('index.html').toString();
+// var parsedHTML = $.load(htmlString);
+//
+// //query for all elements with class 'foo' and loop over them
+// parsedHTML('.foo').map(function(i,foo){
+// //the foo html elements into a cheerio object (same pattern as jQuery)
+//   foo = $(foo);
+//   console.log(foo.text());
+// })
+
+
+// var request = require('request');
+//
+// function gotHTML(err, resp, html) {
+//   if (err) return console.error(err);
+//   var parsedHTML = $.load(html);
+//   // get all img tags and loop over them
+//   var imageURLs = [];
+//   parsedHTML('a').map(function(i, link) {
+//     var href = $(link).attr('href');
+//     if (!href.match('.png')) return;
+//     imageURLs.push(domain + href);
+//
+//   });
+//   console.log(imageURLs);
+// }
+//
+// var domain = 'http://substack.net/images/';
+// request(domain, gotHTML);
+
+
+var request = require('request');
+
+function gotHTML(err, resp, html) {
+  if (err) return console.error(err);
+  var $ = cheerio.load(html);
+  // get all img tags and loop over them
+  var answerURLs = [];
+
+  var answers = $('.ya-q-full-text');
+  var split_answer;
+
+  answers.map(function(i, answer) {
+    var $answer = $(answer);
+    split_answer = $answer.text().split("\n");
+    for (i = 0; i <= split_answer.length; i++) {
+      if (split_answer[i]) {
+        db.yahoo.save({
+          "truthAnswers": split_answer[i]
+        });
+        console.log("sentence " + i + " "+ split_answer[i]);
+      }
+    }
+    // answerURLs.push(split_answr);
+    answerURLs.push($answer.text());
+    // db.truth.save({
+    //   "truthAnswers":split_answer
+    // })
+    console.log("<br>");
+  })
+
+  // console.log(answerURLs[1]);
+  // // answerURLs.push(split_answr);
+  // for (i = 0; i++; i <= split_answer.length) {
+  //   db.truth.save({
+  //     "truthAnswers":split_answer[i]
+  //   });
+
+  //}
+
+}
+
+var domain = 'https://answers.yahoo.com/question/index;_ylt=AwrC0wxDZJxaKVsAthZPmolQ;_ylu=X3oDMTEyYmdmanA3BGNvbG8DYmYxBHBvcwMyBHZ0aWQDQjI1NTlfMQRzZWMDc3I-?qid=20071205083615AAnTUqB';
+request(domain, gotHTML);
---
+++ final/webserver.js
@@ -0,0 +1,116 @@
+//var config = require('./config.js');
+var mongojs = require('mongojs');
+var db = mongojs("cho:woshinaochou4@ds235768.mlab.com:35768/truthbook", ['yahoo']);
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({
+  extended: true
+}); // for parsing form data
+app.use(urlencodedParser);
+
+//Every time the app receives a request, it prints the message “LOGGED” to the terminal.
+// var myLogger = function(req,res,next){
+//   console.log("LOGGED");
+//   next();
+// }
+// //create a cuntion that displays the timestamp of your request in the browser
+// var requestTime = function(req,res,next){
+//   req.requestTime=Date.now();
+//   next();
+// }
+//load the request time function
+// app.use(requestTime);
+// //load the middleware function
+// app.use(myLogger);
+//middleware functions that are loaded first are also executed first.
+
+//set Static path
+app.use(express.static('public'));
+
+//incoporate ejs, we are gonna use ejs as the view engine
+app.set('view engine', 'ejs');
+
+//set routhe for hello world and timestamp
+// app.get('/', function (req, res) {
+//   var responseText = "Hello World!<br>";
+//   responseText+='<small>Requested at:'+req.requestTime+'</small>';
+//   res.send(responseText);
+// })
+
+var count = 0;
+var thesubmissions = [];
+
+
+// app.get('/count', function(req, res) {
+//   //this is middleware
+//   count++;
+//   res.send('<html><body><h1>You recieved' + count + '</h1></body></html>');
+// });
+
+app.get('/formpost', function(req, res) {
+
+  console.log("They submitted:" + req.query.truth);
+  var htmltoSend = "<html><head><link rel=\"stylesheet\" href=\"css/submit.css\" ></head><body><div id=\"showAnswer\"><h1 style=\"margin:auto;width:50%\">You wrote: " + req.query.truth + "</h1><form method=\"GET\" action=\"/\"><button class=\"button\">Back</button></form></div></body></html>";
+  res.send(htmltoSend);
+  thesubmissions.push(req.query.truth);
+  // res.redirect('/test');
+  // });
+
+  db.yahoo.save({
+    "truthAnswers": req.query.truth
+  }, function(err, saved) {
+    if (err || !saved) console.log("Not saved");
+    else console.log("Saved");
+  });
+});
+
+/* Alternatively you could loop through the records with a "for"
+  	for (var i = 0; i < saved.length; i++) {
+	  	console.log(saved[i]);
+	}
+	*/
+
+//use ejs to return pages
+app.get('/display', function(req, res) {
+  db.yahoo.find({}, function(err, saved) {
+    if (err || !saved) {
+      console.log("No results");
+    } else {
+      console.log(saved);
+      res.render('template.ejs', {
+        truthAnswers: saved
+      });
+    }
+  });
+});
+
+app.get('/search', function(req, res) {
+  var query = new RegExp(req.query.key, 'i');
+  db.truth.find({
+      "truthAnswers": query
+    },
+    function(err, saved) {
+      if (err || !saved) {
+        console.log("No results");
+      } else {
+        res.render('template.ejs', {
+          truthAnswers: saved
+        });
+      }
+  });
+});
+
+
+// app.get('/search', function(req, res) {
+//   db.mycollection.find({"attribute":"value_to_search_for"}, function(err, saved) {
+//     if( err || !saved) console.log("No results");
+//     else saved.forEach( function(record) {
+//       console.log(record);
+//     } );
+//   });
+// });
+
+app.listen(8080, function() {
+  console.log('Example app listening on port 3000!');
+})
